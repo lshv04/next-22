16f780b72476dc7a259dc5a0f4f734c7d8586b10
Leandro Hosken <lshv04@gmail.com>
2025-01-08 09:48:31 -0300

    trabalhando link ativo

--- src/components/component/navbar.tsx
+++ src/components/component/navbar.tsx
@@ -4,16 +4,25 @@ import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React, { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname(); // Hook para obter o caminho atual
 
   const handleLinkClick = () => {
     setIsOpen(false); // Fecha o menu ao clicar em um link
   };
 
+  const getLinkClass = (href:string) => {
+    // Retorna a classe 'ativo' se o caminho atual corresponder ao href
+    return pathname === href
+      ? "text-blue-500 font-bold border-b-2 border-blue-500" // Classe ativa
+      : "text-white lg:text-white text-black"; // Texto preto em telas menores e branco em maiores
+  };
+
   return (
-    <header className=" flex  h-20 w-full shrink-0 items-center justify-between px-4 md:px-6 bord bg-gray-900 text-white shadow-lg z-1000">
+    <header className="flex h-20 w-full shrink-0 items-center justify-between px-4 md:px-6 bord bg-gray-900 text-white shadow-lg z-1000">
       <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="lg:hidden bord">
@@ -22,49 +31,54 @@ export function Navbar() {
           </Button>
         </SheetTrigger>
         <SheetContent side="top" className="bord">
-          <Link href="/" className="mr-6  hidden lg:flex bord" prefetch={false}>
-            LH's Movies
-            <span className="sr-only">Leandro´s Logo</span>
-          </Link>
           <div className="grid gap-2 ">
-          <Link
+            <Link
               href="/"
-              className="flex w-full items-center py-2  text-lg font-semibold"
+              className={`flex w-full items-center py-2 text-lg font-semibold ${getLinkClass(
+                "/"
+              )}`}
               prefetch={false}
-              onClick={handleLinkClick} // Fecha o menu ao clicar
+              onClick={handleLinkClick}
             >
               Home
             </Link>
             <Link
               href="/now-playing"
-              className="flex w-full items-center py-2  text-lg font-semibold bord"
+              className={`flex w-full items-center py-2 text-lg font-semibold ${getLinkClass(
+                "/now-playing"
+              )}`}
               prefetch={false}
-              onClick={handleLinkClick} // Fecha o menu ao clicar
+              onClick={handleLinkClick}
             >
               Now Playing
             </Link>
-       
             <Link
               href="/popular"
-              className="flex w-full items-center py-2 text-lg font-semibold"
+              className={`flex w-full items-center py-2 text-lg font-semibold ${getLinkClass(
+                "/popular"
+              )}`}
               prefetch={false}
-              onClick={handleLinkClick} // Fecha o menu ao clicar
+              onClick={handleLinkClick}
             >
               Popular
             </Link>
             <Link
               href="/top-rated"
-              className="flex w-full items-center py-2 text-lg font-semibold"
+              className={`flex w-full items-center py-2 text-lg font-semibold ${getLinkClass(
+                "/top-rated"
+              )}`}
               prefetch={false}
-              onClick={handleLinkClick} // Fecha o menu ao clicar
+              onClick={handleLinkClick}
             >
               Top Rated
             </Link>
             <Link
               href="/upcoming"
-              className="flex w-full items-center py-2 text-lg font-semibold"
+              className={`flex w-full items-center py-2 text-lg font-semibold ${getLinkClass(
+                "/upcoming"
+              )}`}
               prefetch={false}
-              onClick={handleLinkClick} // Fecha o menu ao clicar
+              onClick={handleLinkClick}
             >
               Upcoming
             </Link>
@@ -75,31 +89,39 @@ export function Navbar() {
         LH's Movies
         <span className="sr-only">Leandro´s Logo</span>
       </Link>
-      <nav className="mx-auto hidden lg:flex gap-6 bord ">
+      <nav className="mx-auto hidden lg:flex gap-6 bord">
         <Link
           href="/now-playing"
-          className="group inline-flex h-9 w-max items-center justify-center  px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          className={`group inline-flex h-9 w-max items-center justify-center px-4 py-2 text-sm font-medium transition-colors ${getLinkClass(
+            "/now-playing"
+          )}`}
           prefetch={false}
         >
           Now Playing
         </Link>
         <Link
           href="/popular"
-         className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          className={`group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors ${getLinkClass(
+            "/popular"
+          )}`}
           prefetch={false}
         >
           Popular
         </Link>
         <Link
-          href="top-rated"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="/top-rated"
+          className={`group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors ${getLinkClass(
+            "/top-rated"
+          )}`}
           prefetch={false}
         >
           Top Rated
         </Link>
         <Link
-          href="upcoming"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          href="/upcoming"
+          className={`group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors ${getLinkClass(
+            "/upcoming"
+          )}`}
           prefetch={false}
         >
           Upcoming
@@ -109,7 +131,7 @@ export function Navbar() {
   );
 }
 
-function MenuIcon(props) {
+function MenuIcon(props:any) {
   return (
     <svg
       {...props}
