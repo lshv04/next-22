d97fdb78f9dfaa774a5cbd3b0ea1c42e45e7aad8
Leandro Hosken <lshv04@gmail.com>
2025-01-13 10:38:03 -0300

    trabalhando grid de texto home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,13 +44,18 @@ const Home = async () => {
         <div className="absolute inset-0 bg-black bg-opacity-70"></div>
 
         {/* Content */}
-        <div className="relative z-10 p-8">
-          <h1 className="text-4xl font-bold mb-4">Most Popular Movie</h1>
-          <h2 className="text-2xl font-semibold mb-2">
-            {mostPopularMovie.title}
-          </h2>
-          <p className="mb-4">{mostPopularMovie.overview}</p>
-          <p className="text-lg">Popularity: {mostPopularMovie.popularity}</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-4 mt-14">
+          <div className="relative z-10 p-8 col-span-1 md:col-span-1 lg:col-span-1">
+            <h1 className="text-4xl font-bold mb-4">Most Popular Movie:</h1>
+            <h2 className="text-2xl font-semibold mb-2">
+              {mostPopularMovie.title}
+            </h2>
+            <p className="mb-4">{mostPopularMovie.overview}</p>
+          
+          </div>
+          <div className="col-span-1 md:col-span-1 lg:col-span-3">
+            {/* Outros conteúdos do grid */}
+          </div>
         </div>
       </div>
       <div></div>
